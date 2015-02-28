a10807edd11b37933e6a787a9857775205b67c9b
lavsurgut <lavsurgut@gmail.com>
2015-02-28 18:48:58 +0000

    Update Task.groovy

--- src/main/groovy/lavsurgut/com/release/manager/lib/task/Task.groovy
+++ src/main/groovy/lavsurgut/com/release/manager/lib/task/Task.groovy
@@ -1,1 +1,23 @@
-ddd
+
+package com.ubs.lem.release.manager.lib.task
+
+
+/**
+ * @author Valery Lavrentiev, valeriy.lavrentev@ubs.com
+ *
+ */
+abstract class Task {
+
+	abstract void executeBeforeChecks()
+
+	abstract void executeCommand()
+
+	abstract void executeAfterChecks()
+
+
+	void run () {
+		executeBeforeChecks()
+		executeCommand()
+		executeAfterChecks()
+	}
+}
