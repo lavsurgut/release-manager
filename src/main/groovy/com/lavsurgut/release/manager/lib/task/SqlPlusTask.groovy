70a76f0417d69cf7bd231a84e3b6ee7fe16e86f6
lavsurgut <lavsurgut@gmail.com>
2015-03-13 21:11:34 +0000

    Update SqlPlusTask.groovy

--- src/main/groovy/com/lavsurgut/release/manager/lib/task/SqlPlusTask.groovy
+++ src/main/groovy/com/lavsurgut/release/manager/lib/task/SqlPlusTask.groovy
@@ -1,40 +1,34 @@
-package com.lavsurgut.release.manager.lib.task
+package com.ubs.lem.release.manager.lib.task
 
 import groovy.util.logging.Log4j
-import com.lavsurgut.release.manager.lib.task.Task
+
 
 
 /**
- * @author Valery Lavrentiev, lavsurgut@gmail.com
+ * @author Valery Lavrentiev, valeriy.lavrentev@ubs.com
  *
  */
 @Log4j
 class SqlPlusTask extends Task {
 
 
-	String sqlplusExecutable = "sqlplus"
+	final String sqlplusExecutable = "sqlplus"
 	String user
 	String password
 	String tnsName
 	String dir
 	String script
-	/*String buffer out output*/
+	
 	String sqlPlusOutBuffer
-	/*String buffer err output*/
+	
 	String sqlPlusErrBuffer
 
-	String logResultOutput = ""
+	String logResultOutput 
 
-	SqlPlusTask(String user, String password, String tnsName, String script) {
-		this.user = user
-		this.password = password
-		this.tnsName = tnsName
-		this.script = script
-	}
+	String negativePatternsList
 
 
-
-	private setServiceStmts() {
+	private void setServiceStmts() {
 		//TODO: add in front whenever sqlerror oserror exit rollback
 		int exit_is_there = 0
 		int whenever_clause_is_there = 0
@@ -59,27 +53,27 @@ class SqlPlusTask extends Task {
 
 	private void analyzeLog()
 	{
-		def negativeArray = ["ORA-", "SP2-", "EOF"]
-		def matcher
 
+		def matcher
 		logResultOutput = ""
 
-		negativeArray.each { i->
+
+		negativePatternsList ?:  ["ORA-", "SP2-", "EOF"].each { i->
 			if ((matcher = sqlPlusOutBuffer =~ i))
 				logResultOutput = logResultOutput + "\nNUMBER OF KEY WORD " + i + " is " + matcher.getCount()
 
 		}
-		
+
 		//Check if negative output was not found, raise an exception instead
 		assert(logResultOutput.isEmpty())
 
 	}
-	//@Override
+	@Override
 	void executeCommand()
 	{
 
-		def ant
-		
+		AntBuilder ant
+
 		setServiceStmts()
 
 		ant = new AntBuilder()
@@ -90,29 +84,11 @@ class SqlPlusTask extends Task {
 		executable: "${sqlplusExecutable}") { arg(line: "-s ${user}/${password}@${tnsName} @${script}")}
 
 		sqlPlusErrBuffer = ant.project.properties.cmdErr
-		sqlPlusOutBuffer = ant.project.properties.cmdOut		
-		
+		sqlPlusOutBuffer = ant.project.properties.cmdOut
+
 		log.info sqlplusExecutable + " output: " + "\n\n" + sqlPlusOutBuffer
-		
+
 		analyzeLog()
 	}
-
-
-
-	//@Override
-	public void executeBeforeChecks() {
-		null
-		
-	}
-
-
-
-	//@Override
-	public void executeAfterChecks() {
-		null
-		
-	}
-
-
 
 }
