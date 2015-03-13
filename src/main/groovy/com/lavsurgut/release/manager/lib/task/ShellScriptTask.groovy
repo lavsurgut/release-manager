6440a77b246e77a867e2ef93bc721f7410ac15bc
lavsurgut <lavsurgut@gmail.com>
2015-03-13 21:10:50 +0000

    Update ShellScriptTask.groovy

--- src/main/groovy/com/lavsurgut/release/manager/lib/task/ShellScriptTask.groovy
+++ src/main/groovy/com/lavsurgut/release/manager/lib/task/ShellScriptTask.groovy
@@ -1,1 +1,38 @@
-ddd
+/**
+ * 
+ */
+package com.ubs.lem.release.manager.lib.task
+
+import groovy.util.logging.Log4j
+
+/**
+ * @author Valery Lavrentiev, valeriy.lavrentev@ubs.com
+ *
+ */
+@Log4j
+class ShellScriptTask extends Task {
+
+	String argumentsLine
+	String script
+	String scriptErrBuffer
+	String scriptOutBuffer
+
+	@Override
+	void executeCommand() {
+
+		AntBuilder ant
+
+
+		ant = new AntBuilder()
+		ant.exec(outputproperty: "cmdOut",
+		errorproperty: "cmdErr",
+		resultproperty:"cmdExit",
+		failonerror: "true",
+		executable: "${script}") { arg(line: "${argumentsLine}")}
+
+		scriptErrBuffer = ant.project.properties.cmdErr
+		scriptOutBuffer = ant.project.properties.cmdOut
+
+		log.info "script output: " + "\n\n" + scriptOutBuffer
+	}
+}
