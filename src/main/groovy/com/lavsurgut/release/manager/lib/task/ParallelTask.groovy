75ff22568eb6ff51675cb64120a4890106658861
lavsurgut <lavsurgut@gmail.com>
2015-03-13 21:09:56 +0000

    Update ParallelTask.groovy

--- src/main/groovy/com/lavsurgut/release/manager/lib/task/ParallelTask.groovy
+++ src/main/groovy/com/lavsurgut/release/manager/lib/task/ParallelTask.groovy
@@ -1,5 +1,34 @@
-package com.lavsurgut.release.manager.lib.task
+/**
+ * 
+ */
+package com.ubs.lem.release.manager.lib.task
 
-class ParallelTask {
+import groovy.util.logging.Log4j
+
+/**
+ * @author Valery Lavrentiev, valeriy.lavrentev@ubs.com
+ *
+ */
+@Log4j
+class ParallelTask extends Task {
+	
+	HashMap<String, Task> runnables
+	
+
+	@Override
+	public void executeCommand() {
+		
+		runnables.collect { r -> new Thread(new Runnable() {
+			@Override
+			void run() {
+				log.info "Starting thread"		
+				r.getValue().run()
+				log.info "Finishing thread"
+			}
+		})}.each {t -> t.start()}.each {t -> t.join()}
+		
+	}
+
+
 
 }
